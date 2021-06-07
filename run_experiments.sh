defcde08cbdc5e1acafd11277b7654820ea72200
LJP-luo <1084913834@qq.com>
2021-06-07 11:32:32 +0800

    add this file to run .py for many times

--- run_experiments.sh
+++ run_experiments.sh
@@ -5,48 +5,9 @@
 # chmod +x ./test.sh
 # ./run_experiments.sh
 
-for ((i=0;i<2;i+=1))
-do
-#	python main.py \
-#	--policy "TD3" \
-#	--env "HalfCheetah-v3" \
-#	--seed $i
-#
-#	python main.py \
-#	--policy "TD3" \
-#	--env "Hopper-v3" \
-#	--seed $i
-#
-#	python main.py \
-#	--policy "TD3" \
-#	--env "Walker2d-v3" \
-#	--seed $i
-
-	python ppo_runner.py \
-	--env "Ant-v3" \
-	--seed $i \
-	--epochs 500
-
-#	python main.py \
-#	--policy "TD3" \
-#	--env "Humanoid-v3" \
-#	--seed $i
-#
-#	python main.py \
-#	--policy "TD3" \
-#	--env "InvertedPendulum-v2" \
-#	--seed $i \
-#	--start_timesteps 1000
-#
-#	python main.py \
-#	--policy "TD3" \
-#	--env "InvertedDoublePendulum-v2" \
-#	--seed $i \
-#	--start_timesteps 1000
-#
-#	python main.py \
-#	--policy "TD3" \
-#	--env "Reacher-v2" \
-#	--seed $i \
-#	--start_timesteps 1000
+for (( i = 0; i < 2; i++ )); do
+    python ppo_runner.py \
+    --env "HumanoidStandup-v2" \
+    --seed $i \
+    --epochs 500
 done
